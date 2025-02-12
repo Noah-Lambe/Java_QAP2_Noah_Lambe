6fcb77e4bcd39605afdf0bb8d76b5dbc8efc963e
Noah Lambe <noahalambe@gmail.com>
2025-02-12 12:05:47 -0330

    fixed charge and payment to only execute if logical circumstances occur. (only add payment if it does not exceed limit, only add payment if it does not exceed balance.)

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -23,16 +23,16 @@ public class CreditCard {
     public void charge(Money amount) {
         if (balance.add(amount).compareTo(creditLimit) > 0) {
             System.out.println("Charge exceeds credit limit");
+        } else {
+            balance = balance.add(amount);
         }
-        balance = balance.add(amount);
     }
 
     public void payment(Money amount) {
         if (amount.compareTo(balance) > 0) {
             System.out.println("Payment exceeds balance");
+        } else {
+            balance = balance.subtract(amount);
         }
-        balance = balance.subtract(amount);
     }
-
-
 }
